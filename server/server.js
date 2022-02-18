b84294f7f48718945d8ffee589e3aa9c68018607
Rob Shoemaker <rob.shoe187@gmail.com>
2022-02-18 14:17:57 -0500

    comments server/server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const startServer = async () => {
     typeDefs,
     resolvers,
     context: authMiddleware,
-    plugins: [myPlugin],
+    //plugins: [myPlugin],
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -54,13 +54,13 @@ app.use(express.json());
 // // Serve up static assets
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once("open", () => {
   app.listen(PORT, () => {
