be58f1702a43c322f5fe741f5075d674a200ff16
Rob Shoemaker <rob.shoe187@gmail.com>
2022-03-01 13:30:20 -0500

    updated component logic

--- client/src/pages/Nutrition.js
+++ client/src/pages/Nutrition.js
@@ -1,13 +1,15 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Details from "../components/Details";
 import { QUERY_ALL_NUTRITION } from "../utils/queries";
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Nutrition = () => {
     
     const { loading, data } = useQuery(QUERY_ALL_NUTRITION);
+
+    const [boolDetails] = useState(true);
 
     return(
         <>
